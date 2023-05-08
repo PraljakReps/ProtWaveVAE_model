fbef8f266d54f782851873a84ed548fcb90284d1
Niksa Praljak <niksapraljak1@midway3-login3.rcc.local>
2023-05-08 09:19:57 -0500

    remove version

--- src/__init__.py
+++ src/__init__.py
@@ -5,8 +5,6 @@ from src.model_components import GatedCNN_encoder, TopModel_layer, Decoder_re
 from src.wavenet_decoder import Wave_generator, CondNet
 from src.model_ensemble import ProtWaveVAE, SS_ProtWaveVAE
 
-# version of the realpython-reader package
-__version__ = "0.0.0"
 
 
 
