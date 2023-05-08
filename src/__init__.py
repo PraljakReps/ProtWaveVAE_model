a27b66d907731f1159c9715b7a2f029031a4206b
Niksa Praljak <niksapraljak1@midway3-0320.rcc.local>
2023-05-08 09:02:59 -0500

    update init in src

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
 # __init__.py
 
 from importlib import resources
-from model_components import GatedCNN_encoder, TopModel_layer, Decoder_re
-from wavenet_decoder import Wave_generator
-from model_ensemble import ProtWaveVAE, SS_ProtWaveVAE
+from src.model_components import GatedCNN_encoder, TopModel_layer, Decoder_re
+from src.wavenet_decoder import Wave_generator, CondNet
+from src.model_ensemble import ProtWaveVAE, SS_ProtWaveVAE
 
 # version of the realpython-reader package
 __version__ = "0.0.0"
